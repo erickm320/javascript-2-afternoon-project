ea9b65038139574af901a4ab20144b2653b0f718
erickm320 <erickm320@icloud.com>
2020-09-03 22:06:27 -0600

    finish

--- arrays/practice.js
+++ arrays/practice.js
@@ -18,7 +18,10 @@ var arr = [10,20,30];
 */
 
 //Code Here
+function first(arr){
+  return arr[0]
 
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +36,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+  return arr[2]
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,6 +53,10 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
+function looper(family){
+  for(let i = 0; i < family.length; i++){
+    alert(family[i]);}
+} 
 
 
 
@@ -63,7 +72,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(letters){
+  for(var i = letters.length-1; i>= 0; i--){
+    alert(letters[i])
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,7 +91,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
-
+function evenFinder(nums){
+  for(i=0; i < nums.length; i++ ){
+    if(i % 2===0){
+      nums.splice(i,1)
+    }
+  }
+  return nums;
+}
 
 
 
